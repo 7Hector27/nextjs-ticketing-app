daeb0ad90375eb31f774ab7caccc1fe41d2f777b
Hector Carbajal <hector27c7@gmail.com>
2025-09-11 13:25:43 -0700

    clean up to AuthForm styling and homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,9 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import AuthForm from "@/components/AuthForm";
 
-import UserAPI from "@/lib/UserAPI";
-
 import styles from "./styles.module.scss";
 
 const Home = () => {
-  const userAPI = new UserAPI();
-  React.useEffect(() => {
-    const userAPI = new UserAPI();
-
-    const fetchUsers = async () => {
-      const users = await userAPI.getUsers();
-      console.log(users);
-    };
-    fetchUsers();
-  }, []);
-
   return (
     <div className={styles.home}>
       <Navbar />
